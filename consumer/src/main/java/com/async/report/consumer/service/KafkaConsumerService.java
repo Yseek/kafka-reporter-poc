db6eb98336036d8826afcc43ae3408554fbc1e2d
Yseek <yun5003@naver.com>
2025-07-11 11:33:48 +0900

    :sparkles: consumer 추가... DTO 문제 해결 필요

---
+++ consumer/src/main/java/com/async/report/consumer/service/KafkaConsumerService.java
@@ -0,0 +1,22 @@
+package com.async.report.consumer.service;
+
+import com.async.report.consumer.dto.ReportDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class KafkaConsumerService {
+
+    @KafkaListener(topics = "report-topic", groupId = "report-group")
+    public void consume(ReportDto reportDto) throws InterruptedException {
+        log.info("### 메시지 수신: {}", reportDto);
+
+        // 실제로는 DB 저장, 외부 API 호출 등의 무거운 작업이 이뤄지는 부분
+        // PoC 에서는 5초간 대기하여 오래 걸리는 작업을 시뮬레이션 합니다.
+        log.info("### 데이터 처리 시작...");
+        Thread.sleep(5000); // 5초 대기
+        log.info("### 데이터 치리 완료");
+    }
+}
